2744657c6f7572cb9df1ee8d00bcd381ee38e8b4
rdeshmu <rdeshmukh@walmartlabs.com>
2018-10-24 23:41:36 -0700

    First Commit for Personal Youtube App

---
+++ Youtube/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Rajiv Deshmukh on 10/24/18.
+//  Copyright © 2018 Rajiv Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Home"
+        collectionView.backgroundColor = .white
+    }
+}
+
